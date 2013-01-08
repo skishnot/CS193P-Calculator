495e6e173cb7fa27d76d769a7b66d57af9f95375
Karl <thatkoreanguy1@gmail.com>
2013-01-08 16:11:18 -0500

    Changes as made in Lecture 4. Runs as expected.

--- Calculator/SKCalculatorBrain.h
+++ Calculator/SKCalculatorBrain.h
@@ -14,4 +14,11 @@
 - (void)clearHistory;
 - (double)performOperation:(NSString *)operation;
 
+@property (nonatomic, readonly) id program;
+
++ (double)runProgram:(id)program;
+//pop the top thing off the stack. 1) if the top thing is a number, just return it; 2) if the top thing is an operation, then evaluate it with the next top thing.
++ (NSString *)descriptionOfProgram:(id)program;
+//getter for the program will supply with the program
+
 @end
--- Calculator/SKCalculatorBrain.m
+++ Calculator/SKCalculatorBrain.m
@@ -10,63 +10,91 @@
 
 @interface SKCalculatorBrain ()
 
-@property (nonatomic, strong) NSMutableArray *operandStack;
+@property (nonatomic, strong) NSMutableArray *programStack;
 
 @end
 
 @implementation SKCalculatorBrain
 
-@synthesize operandStack = _operandStack;
+@synthesize programStack = _programStack;
 
 - (NSMutableArray *)operandStack {
-    if (!_operandStack) {
-        _operandStack = [[NSMutableArray alloc] init];
+    if (!_programStack) {
+        _programStack = [[NSMutableArray alloc] init];
     }
-    return _operandStack;
+    return _programStack;
 }
 
 - (void)setOperandStack:(NSMutableArray *)anArray {
-    _operandStack = anArray;
+    _programStack = anArray;
 }
 
 - (void)pushOperand:(double)operand {
     NSNumber *operandObject = [NSNumber numberWithDouble:operand];
-    [self.operandStack addObject:operandObject];
+    [self.programStack addObject:operandObject];
 }
 
-- (double)popOperand {
-    NSNumber *operandObject = [self.operandStack lastObject];
-    if (operandObject) [self.operandStack removeLastObject];
-    return [operandObject doubleValue];
-}
 
 - (double)performOperation:(NSString *)operation {
-    
+    [self.programStack addObject:operation];
+    return [SKCalculatorBrain runProgram:self.programStack];
+    //popOperand is gone now.
+}
+
+- (id)program {
+    //since the property is readonly, only the getter method is needed;
+    //we're making a copy of it, because we can't just go about handing out our internal state.
+    return [self.programStack copy];
+}
+
++ (NSString *)descriptionOfProgram:(id)program {
+    return @"Implement this in Assignment 2";
+}
+
++ (double)popOperandOffStack:(NSMutableArray *)stack {
     double result = 0;
     
-    if ([operation isEqualToString:@"+"]) {
-        result = [self popOperand] + [self popOperand];
-    } else if ([@"*" isEqualToString:operation]) {
-        result = [self popOperand] * [self popOperand];
-    } else if ([operation isEqualToString:@"-"]) {
-        double subtrahend = [self popOperand];
-        result = [self popOperand] - subtrahend;
-    } else if ([operation isEqualToString:@"/"]) {
-        double divisor = [self popOperand];
-        if (divisor) result = [self popOperand] / divisor;
-    } else if ([operation isEqualToString:@"sin"]) {
-        result = sin([self popOperand]);
-    } else if ([operation isEqualToString:@"cos"]) {
-        result = cos([self popOperand]);
-    } else if ([operation isEqualToString:@"sqrt"]) {
-        result = sqrt([self popOperand]);
-    } else if ([operation isEqualToString:@"π"]) {
-        result = M_PI;
+    id topOfStack = [stack lastObject];
+    if (topOfStack) [stack removeLastObject];
+    
+    if ([topOfStack isKindOfClass:[NSNumber class]]) {
+        result = [topOfStack doubleValue];
+    }
+    else if ([topOfStack isKindOfClass:[NSString class]]) {
+        NSString *operation = topOfStack;
+        if ([operation isEqualToString:@"+"]) {
+            result = [self popOperandOffStack:stack] + [self popOperandOffStack:stack];
+        } else if ([@"*" isEqualToString:operation]) {
+            result = [self popOperandOffStack:stack] * [self popOperandOffStack:stack];
+        } else if ([operation isEqualToString:@"-"]) {
+            double subtrahend = [self popOperandOffStack:stack];
+            result = [self popOperandOffStack:stack] - subtrahend;
+        } else if ([operation isEqualToString:@"/"]) {
+            double divisor = [self popOperandOffStack:stack];
+            if (divisor) result = [self popOperandOffStack:stack] / divisor;
+        } else if ([operation isEqualToString:@"sin"]) {
+            result = sin([self popOperandOffStack:stack]);
+        } else if ([operation isEqualToString:@"cos"]) {
+            result = cos([self popOperandOffStack:stack]);
+        } else if ([operation isEqualToString:@"sqrt"]) {
+            result = sqrt([self popOperandOffStack:stack]);
+        } else if ([operation isEqualToString:@"π"]) {
+            result = M_PI;
+        }
     }
     
-    [self pushOperand:result];
-                    
     return result;
+}
+
++ (double)runProgram:(id)program {
+    //always do introspection when using an argument of type id, because god knows what may be passed on to this method, and we do not want to crash our app by passing an argument that the method can't handle.
+    NSMutableArray *stack; //automatically assigned 0
+    if ([program isKindOfClass:[NSArray class]]) {
+        stack = [program mutableCopy];
+    } // the array has to be mutable, so that we can munch on it and the recursion has an end.
+    
+    //I need a method that pops the top thing off the stack
+    return [self popOperandOffStack:stack];
 }
 
 - (void)clearHistory {
